e860288615d666df0dc2199da599266a572e8390
Jeremy Hummel <jehummel@cisco.com>
2012-07-18 18:10:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeTaskManager::Application.config.secret_token = '8b1d2df4132ab70719a823211ec20a70f049cb1d905be7cf92129147aff37595532dd95c1dffe5fc34a35d571b0862a8b6b9abadcfc029c3e25300fb637ec03b'
